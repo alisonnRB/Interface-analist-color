b4fe8cf30e6cfdb0fecee8b8860d8c46d60bb766
alisonnRB <alisonrbjs@gmail.com>
2024-03-24 15:33:14 -0300

    vue project configured

--- vite.config.js
+++ vite.config.js
@@ -14,5 +14,10 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  css: {
+    preprocessorOptions:{
+      scss: { additionalData: '@import "src/scss/index.scss;"'},
+    }
   }
 })
